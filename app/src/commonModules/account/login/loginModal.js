e40915a3d0fffa11e1cf8459c126bd3b54d06509
Petru Isfan <petru.isfan@gmail.com>
2013-12-22 17:57:44 +0200

    Colected login forms into a model

--- app/src/commonModules/account/login/loginModal.js
+++ app/src/commonModules/account/login/loginModal.js
@@ -3,11 +3,16 @@ asdep.directive("loginModal", function() {
     return {
         templateUrl: "src/commonModules/account/login/loginModal.html",
         link: function(scope) {
+            scope.user = {};
+            scope.account = {};
+
             scope.login = function() {
-                console.log("Login");
+                console.log(scope.user);
+                $("#loginModal").modal("hide");
             }
             scope.createAccount = function() {
-                console.log("Create account");
+                console.log(scope.account);
+                $("#loginModal").modal("hide");
             }
         }
     }
